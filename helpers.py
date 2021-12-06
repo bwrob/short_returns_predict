bbf49c19e398d64585555e6444f8e692acc91c70
bwrob <bw2002@wp.pl>
2021-12-06 07:13:48 +0100

    [ADD] logistic regression model

--- helpers.py
+++ helpers.py
@@ -3,8 +3,8 @@ import numpy as np
 import pandas as pd
 import yfinance as yf
 
-
 # Gather hard-coded variables
+# TODO CapsLock to the variables
 flt_market_cap_limit = 10E11
 str_default_ticker = "GME"
 str_default_period = "5y"
@@ -13,11 +13,11 @@ str_csv_format = "_historical_data.csv"
 
 
 def get_ticker_historical_data(ticker_code,
-                               data_period = str_default_period,
+                               data_period=str_default_period,
                                use_persisted_data=True,
                                persist_data=False):
     """
-    Function fetches daily historical price data for the given ticker.f
+    Function fetches daily historical price data for the given ticker.
     The datasource is either live Yahoo Finance API or data persisted in csv.
     :param ticker_code: str
         Equity asset ticker code.
@@ -35,18 +35,20 @@ def get_ticker_historical_data(ticker_code,
         raise Exception("Can't load and stream data at the same time.")
 
     if use_persisted_data:
-        historical_data = pd.read_csv(ticker_code + '_historical_data.csv')
+        historical_data = pd.read_csv(ticker_code + str_csv_format)
     else:
         ticker = yf.Ticker(ticker_code)
-        if ticker.info['marketCap'] > flt_market_cap_limit:
-            raise Exception("This analysis is intended for assets with market cap up to " + str(flt_market_cap_limit) +
-                            ". Current asset market cap is " + ticker.info['marketCap'] + " choose different ticker.")
+        market_cap = ticker.info['marketCap']
+        if market_cap > flt_market_cap_limit:
+            raise Exception(f"""This analysis is intended for assets with market cap up to {flt_market_cap_limit}. 
+            Current asset market cap is {market_cap}, choose different ticker.""")
         historical_data = ticker.history(period=data_period, interval=str_interval, rounding=True)
+        # TODO historical_data = historical_data[["Open", "High", "Low", "Close", "Volume"]]
         if persist_data:
             historical_data.to_csv(ticker_code + str_csv_format)
     return historical_data
 
-
+# TODO change default argument values to unmutable type (tuple?)
 def add_statistics(historical_data,
                    use_sign=True,
                    use_move=True,
@@ -54,14 +56,16 @@ def add_statistics(historical_data,
                    sma_windows=[15, 30, 100, 200],
                    ema_alphas=[0.5, 0.25],
                    momentum_windows=[5, 15, 30]):
-
     # Extract only relevant columns and store in temporary data frame
-    df = historical_data[["Open", "High", "Low", "Close", "Volume"]]
+    # TODO remove redundant df -> df = historical_data.copy() <- always use a copy to avoid warnings
+    df = historical_data[["Open", "High", "Low", "Close", "Volume"]].copy()
 
-    # Add daily price returns and sign
+    #TODO clear up returns
+    # Add daily price returns, future return and sign
     df["Return"] = df["Close"].pct_change(1)
     if use_sign:
-        df["Sign"] = np.log(df["Return"].copy() + 1)
+        df["Sign"] = np.log(df["Return"] + 1)
+    df["Return_future"] = df["Return"].shift(-1)
 
     # Add daily price move and range
     if use_move:
@@ -71,24 +75,24 @@ def add_statistics(historical_data,
 
     # Add momentum statistics
     for window in momentum_windows:
-        df["SMA_" + str(window)] = df["Close"].rolling(window=window).mean()
+        df[f"SMA_{window}"] = df["Close"].rolling(window=window).mean()
 
     # Add simple moving average statistics
     for window in sma_windows:
-        df["SMA_" + str(window)] = df["Close"].rolling(window=window).mean()
+        df[f"SMA_{window}"] = df["Close"].rolling(window=window).mean()
 
     # Add exponential moving average statistics
     # adjust parameter = False to conform with requirements as per pandas.emw specification
     for alpha in ema_alphas:
-        df["SMA_" + str(alpha)] = df["Close"].ewm(alpha=alpha, adjust=False).mean()
+        df[f"EMA_{alpha}"] = df["Close"].ewm(alpha=alpha, adjust=False).mean()
 
-    return df
+    return df.dropna()
 
 
-def categorize_data(statistics_data, low_cutoff, high_cutoff):
-    df = statistics_data[(statistics_data["Return"] >= low_cutoff) & (statistics_data["Return"] <= high_cutoff)]
-    df["Return_direction"] = np.where(df["Return"] > 0, 1, 0)
-    return df
+def categorize_return_data(statistics_data, low_cutoff, high_cutoff):
+    df = statistics_data[(statistics_data["Return_future"] >= high_cutoff) | (statistics_data["Return_future"] <= low_cutoff)].copy()
+    df["Return_direction"] = np.where(df["Return_future"] > 0, 1, 0)
+    return df["Return_direction"]
 
 
 if __name__ == '__main__':
--- main.py
+++ main.py
@@ -1,20 +1,38 @@
-import helpers as hlp
-import matplotlib.pyplot as plt
+import sklearn as skl
+import sklearn.linear_model as skl_lm
+import sklearn.model_selection as skl_ms
+from sklearn.preprocessing import StandardScaler
 
+import helpers as hlp
 
 if __name__ == '__main__':
-    # Workflow of the main script:
+
     # 1. fetch or load historical data for the ticker
-    # 2. calculate specified statistics
-    # 3. visualize the data
-    # 4. perform normalization of predictor variables
-    # 5. fit scikit logistic regression models
+    data_historical = hlp.get_ticker_historical_data("GME", use_persisted_data=True)
 
-    # 1.
-    historical_data = hlp.get_ticker_historical_data("GME", use_persisted_data=True)
-    # 2.
-    statistics_data = hlp.add_statistics(historical_data, sma_windows=[5, 30, 100], ema_alphas=[0.5])
+    # 2. calculate all specified statistics
+    data_statistics = hlp.add_statistics(data_historical, sma_windows=[5, 30, 100], ema_alphas=[0.5, 0.25])
 
-    # 3.
-    statistics_data.plot.area(figsize=(12, 2), subplots=True, stacked=False, layout=(-1, 2))
-    plt.show()
+    # 3. visualize the data and statistics
+    # statistics_data.plot.line(figsize=(20, 12), subplots=True, stacked=False, layout=(-1, 3))
+    # plt.show()
+
+    # 4. process the explanatory and signal data
+    # choose relevant features and normalize the data
+    feature_names = ["Volume", "Sign", "Move", "Range", "SMA_5", "SMA_30", "SMA_100", "EMA_0.25"]
+    X = data_statistics[feature_names]
+    scaler = StandardScaler()
+    X_scaled = scaler.fit_transform(X)
+    # categorize signal data
+    y_categorized = hlp.categorize_return_data(data_statistics, 0.0, 0.0)
+    y_categorized = y_categorized
+    # split data randomly to train and test population
+    X_train, X_test, y_train, y_test = skl_ms.train_test_split(X_scaled, y_categorized, test_size=0.25, random_state=1)
+
+    # 6.
+    logistic_regression_model = skl_lm.LogisticRegression()
+    logistic_regression_model.fit(X_train, y_train)
+    y_prediction = logistic_regression_model.predict(X_test)
+    print("Accuracy:", skl.metrics.accuracy_score(y_test, y_prediction))
+    print("Precision:", skl.metrics.precision_score(y_test, y_prediction))
+    print("Recall:", skl.metrics.recall_score(y_test, y_prediction))
